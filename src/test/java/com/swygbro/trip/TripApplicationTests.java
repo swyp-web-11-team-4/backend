4cbb345ff6df2226896e2aa3f94372f72e2c4ce5
kjyy08 <kjyy08@naver.com>
2025-10-08 19:46:21 +0900

    test: test 프로필만 활성화되도록 수정

--- src/test/java/com/swygbro/trip/TripApplicationTests.java
+++ src/test/java/com/swygbro/trip/TripApplicationTests.java
@@ -3,6 +3,9 @@ package com.swygbro.trip;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
 @SpringBootTest
 class TripApplicationTests {
 
